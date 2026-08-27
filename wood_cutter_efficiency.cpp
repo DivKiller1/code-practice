dad0e34181276064c4cd22bcad6e25efd65a7459
Divyanshu Gaur <90320119+DivKiller1@users.noreply.github.com>
2026-08-27 15:49:29 +0530

    Delete wood_cutter_efficiency.cpp

--- wood_cutter_efficiency.cpp
+++
@@ -1,7 +0,0 @@
-// Problem: Wood Cutter Efficiency
-// Difficulty: Medium
-// Topic: binary search
-//
-// Description:
-// You are given an array of N integers representing the heights of trees, and an integer M 
-//
